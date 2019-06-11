2a34961c01bbb1852877b9826b700cd2987c3841
m.gaertner <m.gaertner@CVB0090>
2019-06-11 15:18:55 +0200

    Projektdateien hinzufügen.

---
+++ SchereSteinPapier_WPF/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchereSteinPapier_WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Spiellogic myLogic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myLogic = new Spiellogic();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            buttonStein.IsEnabled = false;
+            buttonPapier.IsEnabled = false;
+            macheZug();
+        }
+
+
+
+        private void macheZug()
+        {
+            switch (myLogic.macheSpielzug())
+            {
+                case 1:
+                    label1.Content = "Schere";
+                    break;
+
+                case 2:
+                    label1.Content = "Stein";
+                    break;
+
+                case 3:
+                    label1.Content = "Papier";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void ButtonStein_Click(object sender, RoutedEventArgs e)
+        {
+            buttonSchere.IsEnabled = false;
+            buttonPapier.IsEnabled = false;
+            macheZug();
+        }
+
+        private void ButtonPapier_Click(object sender, RoutedEventArgs e)
+        {
+            buttonSchere.IsEnabled = false;
+            buttonStein.IsEnabled = false;
+            macheZug();
+        }
+    }
+}
---
+++ SchereSteinPapier_WPF/Spiellogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchereSteinPapier_WPF
+{
+    class Spiellogic
+    {
+        // 0 = nicht gezogen, 1 = Schere, 2 = Stein, 3 = Papier
+        private int Computerzug { get; set; }
+        private int Spielerzug { get; set; }
+        private int PunkteComputer { get; set; }
+        private int PunkteSpieler { get; set; }
+
+        public Spiellogic()
+        {
+            Computerzug = 0;
+            Spielerzug = 0;
+            PunkteComputer = 0;
+            PunkteSpieler = 0;
+        }
+
+        internal int macheSpielzug()
+        {
+            Random random = new Random();
+            return(random.Next(1, 4));
+        }
+    }
+}
